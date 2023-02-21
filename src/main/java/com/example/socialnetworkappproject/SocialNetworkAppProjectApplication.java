a91cec5ab1345d20f53b7e6ade68e3fb49adb297
bojo8 <bojo856@gmail.com>
2023-02-21 18:17:20 +0100

    Create repository

---
+++ src/main/java/com/example/socialnetworkappproject/SocialNetworkAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialnetworkappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkAppProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socialnetworkappproject/SocialNetworkAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialnetworkappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
