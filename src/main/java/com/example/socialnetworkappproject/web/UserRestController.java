00501aab0e8cdbcdbedcf5498ffe70e46625b3d7
bojo8 <bojo856@gmail.com>
2023-02-22 21:50:36 +0100

    Create RestController And Start Web

--- src/main/java/com/example/socialnetworkappproject/web/UserRestController.java
+++ src/main/java/com/example/socialnetworkappproject/web/UserRestController.java
@@ -2,6 +2,8 @@ package com.example.socialnetworkappproject.web;
 
 
 import com.example.socialnetworkappproject.model.User;
+import com.example.socialnetworkappproject.service.UserSerivce;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserRestController {
+
+    private final UserSerivce serivce;
 
     @GetMapping
     public List<User> getUser(){
-        return
+        return serivce.findAll();
     }
 }
